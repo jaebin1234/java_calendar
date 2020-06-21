8167f80692346c914fd853e00f611ec81cb8e210
jaebin1234 <jaebin2586@gmail.com>
2020-06-22 04:36:25 +0900

    캘린더를 만들어보자_5

--- src/com/example/Prompt.java
+++ src/com/example/Prompt.java
@@ -4,17 +4,31 @@ import java.util.Scanner;
 /*
  * 월을 입력하면 해당월의 달력을 출력한다.
  * 달력의 모양은 1단계에서 작성한 모양으로 만든다.
- * 2020년 1월 모델을 예시로 만든다.
+ * 1일은 일요일로 정해도 무방하다.
  * -1을 입력받기 전까지 반복입력을 받는다.
  * 프롬프트를 출력한다.
  * */
 
 public class Prompt {
-
-
-    private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-    private final static String PROMPT = "cal>> ";
+    public int parseDay(String week) {
+        if (week.equals("일요일")) { //문자열 비교는 equals
+            return 0;
+        } else if (week.equals("월요일")) {
+            return 1;
+        } else if (week.equals("화요일")) {
+            return 2;
+        } else if (week.equals("수요일")) {
+            return 3;
+        } else if (week.equals("목요일")) {
+            return 4;
+        } else if (week.equals("금요일")) {
+            return 5;
+        } else if (week.equals("토요일")) {
+            return 6;
+        } else {
+            return 0;
+        }
+    }
 
     public void runPrompt() {
         java_calendar_5 cal = new java_calendar_5(); //생성자로 객체 만들기
@@ -22,32 +36,44 @@ public class Prompt {
         Scanner sc = new Scanner(System.in);
 
         int month = 1;
+        int year = 2020;
+        int weekday = 0;
+
 
         while (true) {
-            System.out.println("입력 하고 싶은 월을 입력 하시오(종료는 -1) : ");
-            System.out.print(PROMPT);
-            month = sc.nextInt();
+            System.out.println("입력 하고 싶은 년도를 입력 하시오(종료는 -1) : ");
+            System.out.print("YEAR> ");
+            year = sc.nextInt();
+            if (year == -1) {
+                break;
+            }
 
+
+            System.out.println("입력 하고 싶은 월을 입력 하시오(종료는 -1) : ");
+            System.out.print("MONTH> ");
+            month = sc.nextInt();
             if (month == -1) {
                 break;// month가 -1이면 이 while문을 빠져나와라.
             }
-            if (month > 12) {
-                System.out.println("월은 12월 까지 있습니다. 다시 입력해 주세요. ");
+            if (month > 12 || month < 1) {
+                System.out.println("잘못된 입력입니다. 다시 입력해 주세요. ");
                 continue; //month가 12이상이면 while문을 다시시작.
             }
-            if (month >= 1 && month <= 12) {
 
-                cal.printCalendar_sample();
-                System.out.println(" " + month + "월은 " + cal.dayMethod(month) + "일까지 있습니다.");
-            }
+            System.out.println("첫째 날의 요일을 입력하세요(일요일,월요일,화요일,수요일,목요일,금요일.토요일) : ");
+            String str_weekday = sc.next();
+            weekday = parseDay(str_weekday);
+
+
+            cal.printCalendar_sample(year, month, weekday);//달력
+            System.out.println(" " + month + "월은 " + cal.dayMethod(year, month) + "일까지 있습니다.");
+            System.out.println();
+
         }
         System.out.println("달력 프로 그램이 종료 됩니다.");
         sc.close();
     }
 
-    public int dayMethod(int n) {
-        return MAX_DAYS[n - 1];
-    }
 
     public static void main(String[] args) {
         //셀 실행
@@ -57,13 +83,3 @@ public class Prompt {
     }
 
 }
-/* 2020년 1월달력
-        System.out.println(" ******2020-01******");
-        System.out.println(" 일 월 화  수  목 금 토");
-        System.out.println(" ---------------------");
-        System.out.println(" -  -  -  1  2  3  4");
-        System.out.println(" 5  6  7  8  9  10 11");
-        System.out.println(" 12 13 14 15 16 17 18");
-        System.out.println(" 19 20 21 22 23 24 25");
-        System.out.println(" 26 27 28 29 30 31");
- */
--- src/com/example/java_calendar_5.java
+++ src/com/example/java_calendar_5.java
@@ -1,63 +1,86 @@
 package com.example;
 
 import java.util.Scanner;
-/*
-* 월을 입력하면 해당월의 달력을 출력한다.
-* 달력의 모양은 1단계에서 작성한 모양으로 만든다.
-* 2020년 1월 모델을 예시로 만든다.
-* -1을 입력받기 전까지 반복입력을 받는다.
-* 프롬프트를 출력한다.
-* */
 
-public class java_calendar_5 {
-    public void printCalendar_sample(){
-        System.out.println(" ******2020-01******");
+/* 2020년 1월달력
+        System.out.println("   ****2020-01****");
         System.out.println(" 일 월 화  수  목 금 토");
-        System.out.println(" --------------------");
+        System.out.println(" ---------------------");
         System.out.println(" -  -  -  1  2  3  4");
         System.out.println(" 5  6  7  8  9  10 11");
         System.out.println(" 12 13 14 15 16 17 18");
         System.out.println(" 19 20 21 22 23 24 25");
-        System.out.println(" 26 27 28 29 30 31 ");
-        System.out.println(" --------------------");
+        System.out.println(" 26 27 28 29 30 31");
+ */
+/*
+ * 월을 입력하면 해당월의 달력을 출력한다.
+ * 달력의 모양은 1단계에서 작성한 모양으로 만든다.
+ * 2020년 1월 모델을 예시로 만든다.
+ * -1을 입력받기 전까지 반복입력을 받는다.
+ * 프롬프트를 출력한다.
+ * */
 
-    }
-    private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+public class java_calendar_5 {
 
-    public int dayMethod(int n) {
-        return MAX_DAYS[n - 1];
-    }
+    public void printCalendar_sample(int year, int month, int weekday) {
+        System.out.println("  **** " + year + "년 " + month + "월 ****");
+        System.out.println("  일 월 화  수  목 금 토");
+        System.out.println("  --------------------");
 
-    public static void main(String[] args) {
-        java_calendar_5 cal = new java_calendar_5(); //생성자로 객체 만들기
+        //첫번째 줄에서 달력을 프린트 했을때 빈 공간
+        for (int i = 0; i < weekday; i++) {
+            System.out.print("   ");
+        }
 
-        String PROMPT = "cal> ";
+        int maxDay = dayMethod(year, month);//해당 월의 최대 일수
 
-        Scanner sc = new Scanner(System.in);
+        //첫번재 줄에서 달력을 프린트 했을때 숫자가 시작되는 부분
+        int count = 7 - weekday;
+        int delim = (count < 7) ? count : 0;
+        // if(count<7){
+        // delim = count ;
+        // }else {
+        // delim =0;
+        // }
+        for (int i = 1; i <= count; i++) {
+            System.out.printf("%3d", i);
+        }
+        System.out.println();
+        //두번째줄 ~
 
 
-        int month =1;
-        while (true){
-            System.out.println("입력 하고 싶은 월을 입력 하시오(종료는 -1) : ");
-            System.out.print(PROMPT);
-            month = sc.nextInt();
-
-            if(month==-1){
-                break;// month가 -1이면 이 while문을 빠져나와라.
-            }
-            if(month>12){
-                System.out.println("월은 12월 까지 있습니다. 다시 입력해 주세요. ");
-                continue; //month가 12이상이면 while문을 다시시작.
-            }
-            if(month>=1 && month <=12) {
-
-                cal.printCalendar_sample();
-                System.out.println(" "+month + "월은 " + cal.dayMethod(month) + "일까지 있습니다.");
+        for (int i = count + 1; i <= maxDay; i++) {
+            System.out.printf("%3d", i);
+            if (i % 7 == delim) {
+                System.out.println();
             }
         }
-        System.out.println("달력 프로 그램이 종료 됩니다.");
-        sc.close();
+        System.out.println();
+        System.out.println(" --------------------");
+
+
     }
+
+    private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private static final int[] LEAP_MAX_DAYS = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    public boolean isLeapYear(int year) {
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public int dayMethod(int year, int month) { // 각 월이 몇일까지 있는지
+        if (isLeapYear(year)) {
+            return LEAP_MAX_DAYS[month - 1];
+        } else {
+            return MAX_DAYS[month - 1];
+        }
+    }
+
 
 }
 /* 2020년 1월달력
